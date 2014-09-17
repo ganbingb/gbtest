b082126003215d1645a00cc138dcd1bc862b91ad
ganbingb <allein@sina.com>
2014-09-17 22:02:22 +0800

    xxxx

---
+++ test_bitwidth.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#if ( (sizeof(int) > 32) ? 1 : 0)
+	#define CPU32_BIT 1
+#else
+	#define CPU32NI_BIT
+#endif /*endof bitwidth define*/
+
+void main(void)
+{
+#if CPU32_BIT
+	printf("bits 32\n");
+#else
+	printf("^ @ ^\n");
+#endif /*endof printf cpu bits*/
+}
+
